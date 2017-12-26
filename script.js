78e78159938c590a37310e9cf7e4f3586254068e
jeremyfell <jeremyafell@gmail.com>
2017-12-26 14:54:52 -0800

    Modified variables names

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var wiki = "http://en.wikipedia.org/wiki/";
 
-var esymbol = [
+var element_symbol = [
   " ","H","He","Li","Be","B","C","N","O","F","Ne",
   "Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca",
   "Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn",
@@ -15,7 +15,7 @@ var esymbol = [
 	"Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"
 ];
 
-var ename = [
+var element_name = [
   " ","Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon",
   "Nitrogen","Oxygen","Fluorine","Neon","Sodium","Magnesium",
   "Aluminium","Silicon","Phosphorus","Sulfur","Chlorine","Argon",
@@ -39,13 +39,13 @@ var ename = [
 ];
 
 //Double check all mass values. Quite a few are probably wrong
-var emass = [
-    " ","1.0079","4.0026","6.941","9.0122","10.811","12.011","14.007","15.999","18.998","20.180",
+var element_mass = [
+  " ","1.0079","4.0026","6.941","9.0122","10.811","12.011","14.007","15.999","18.998","20.180",
 	"22.990","24.305","26.982","28.086","30.974","32.066","35.453","39.948","39.098","40.078",
-    "44.956","47.867","50.942","51.996","54.938","55.845","58.933","58.693","63.546","65.38",
-    "69.732","72.631","74.922","78.971","79.904","83.798","85.468","87.62","88.906","91.224",
-    "92.906","95.95","98.907","101.07","102.906","106.42","107.868","112.414","114.818","118.711",
-    "121.760","127.6","126.904","131.29","132.905","137.327","138.906","140.115","140.908","144.24",
+  "44.956","47.867","50.942","51.996","54.938","55.845","58.933","58.693","63.546","65.38",
+  "69.732","72.631","74.922","78.971","79.904","83.798","85.468","87.62","88.906","91.224",
+  "92.906","95.95","98.907","101.07","102.906","106.42","107.868","112.414","114.818","118.711",
+  "121.760","127.6","126.904","131.29","132.905","137.327","138.906","140.115","140.908","144.24",
 	"144.913","150.36","161.966","157.25","158.925","162.50","164.930","167.26","168.934","173.04",
 	"174.967","178.49","180.948","183.85","168.207","190.23","192.22","195.08","196.967","200.59",
 	"204.383","207.2","208.980","[208.982]","209.987","222.018","223.020","226.025","227.028","232.038",
@@ -54,27 +54,28 @@ var emass = [
 	"[272]","[277]","?","[289]","?","[298]","?","?"
 ];
 
-var egroup = [
+//
+var element_group = [
 	9,
-    6,8,
-    0,1,5,6,6,6,7,8,
-    0,1,4,5,6,6,7,8,
-    0,1,3,3,3,3,3,3,3,3,3,3,4,5,5,6,7,8,
-    0,1,3,3,3,3,3,3,3,3,3,3,4,4,5,5,7,8,
-    0,1,2,2,2
+  6,8,
+  0,1,5,6,6,6,7,8,
+  0,1,4,5,6,6,7,8,
+  0,1,3,3,3,3,3,3,3,3,3,3,4,5,5,6,7,8,
+  0,1,3,3,3,3,3,3,3,3,3,3,4,4,5,5,7,8,
+  0,1,2,2,2
 ];
 
 var boxcolor = [
-	["#62a73b", "#72bf44"],
-	["#009353", "#00a65d"],
-	["#512480", "#5c2d91"],
-	["#ccbe00", "#e3d200"],
-	["#1c3687", "#21409a"],
-	["#d99116", "#faa61a"],
-	["#00599d", "#0066b3"],
-	["#ce181e", "#ed1c24"],
-	["#8f187c", "#a3238e"],
-	["#7f8c8d", "#bdc3c7"]
+	["#62a73b", "#72bf44"], //
+	["#009353", "#00a65d"], //
+	["#512480", "#5c2d91"], //
+	["#ccbe00", "#e3d200"], //
+	["#1c3687", "#21409a"], //
+	["#d99116", "#faa61a"], //
+	["#00599d", "#0066b3"], //
+	["#ce181e", "#ed1c24"], //
+	["#8f187c", "#a3238e"], //
+	["#7f8c8d", "#bdc3c7"]  //
 ];
 
 var state = ["off", "on"];
@@ -167,24 +168,24 @@ function update(repeat) {
 				}
 			}
 			ap.innerHTML = 95 - 34 * Number(post);
-			sp.innerHTML = esymbol[95 - 34 * Number(post)];
-			np.innerHTML = ename[95 - 34 * Number(post)];
-			mp.innerHTML = emass[95 - 34 * Number(post)];
+			sp.innerHTML = element_symbol[95 - 34 * Number(post)];
+			np.innerHTML = element_name[95 - 34 * Number(post)];
+			mp.innerHTML = element_mass[95 - 34 * Number(post)];
 
 		} else {
 			ep.style.visibility = "hidden";
 		}
-		sh.innerHTML = esymbol[h];
-		sm.innerHTML = esymbol[m];
-		ss.innerHTML = esymbol[s];
+		sh.innerHTML = element_symbol[h];
+		sm.innerHTML = element_symbol[m];
+		ss.innerHTML = element_symbol[s];
 		ah.innerHTML = h;
 		am.innerHTML = m;
 		as.innerHTML = s;
 
 		if (sname) {
-			nh.innerHTML = ename[h];
-			nm.innerHTML = ename[m];
-			ns.innerHTML = ename[s];
+			nh.innerHTML = element_name[h];
+			nm.innerHTML = element_name[m];
+			ns.innerHTML = element_name[s];
 			showElements([nh, nm, ns]);
 			sfull ? np.style.visibility = "hidden" : np.style.visibility = "visible";
 		} else {
@@ -192,9 +193,9 @@ function update(repeat) {
 		}
 
 		if (smass) {
-			mh.innerHTML = emass[h];
-			mm.innerHTML = emass[m];
-			ms.innerHTML = emass[s];
+			mh.innerHTML = element_mass[h];
+			mm.innerHTML = element_mass[m];
+			ms.innerHTML = element_mass[s];
 			showElements([mh, mm, ms])
 			sfull ? mp.style.visibility = "hidden" : mp.style.visibility = "visible";
 		} else {
@@ -202,9 +203,9 @@ function update(repeat) {
 		}
 
 		if (sgroup) {
-			groupUpdate(egroup[h], eh);
-			groupUpdate(egroup[m], em);
-			groupUpdate(egroup[s], es);
+			groupUpdate(element_group[h], eh);
+			groupUpdate(element_group[m], em);
+			groupUpdate(element_group[s], es);
 			groupUpdate(2, ep);
 		} else {
 			groupDefault(eh);
@@ -318,9 +319,9 @@ function changeDisplay(cdisplay) {
 for (var n = 1; n <= 118; n++) {
 	var celement = document.getElementById("e" + n);
 	celement.childNodes[0].innerHTML = n;
-	celement.childNodes[1].innerHTML = esymbol[n];
-	celement.onclick = function() {window.open(wiki + ename[Number(this.childNodes[0].innerHTML)])};
-	celement.title = ename[n];
+	celement.childNodes[1].innerHTML = element_symbol[n];
+	celement.onclick = function() {window.open(wiki + element_name[Number(this.childNodes[0].innerHTML)])};
+	celement.title = element_name[n];
 	elements.push(document.getElementById("e" + n));
 }
 
